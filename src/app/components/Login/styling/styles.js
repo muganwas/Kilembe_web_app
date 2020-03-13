38d75cb33e2a5317c91365d336ca43f47b70622e
Muganwa <stevenmuganwa@gmail.com>
2020-03-13 17:10:59 +0300

    converting login component to react native web

---
+++ src/app/components/Login/styling/styles.js
@@ -0,0 +1,60 @@
+//import { StyleSheet } from 'react-native';
+
+const styles = {
+  submitButton: {
+    display: 'flex',
+    width: 250,
+    padding: 3,
+    marginTop: 2,
+    boxSizing: 'border-box',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderRadius: 3,
+    boxShadow: "1px 2px 3px rgba(127,125,135, 0.8)"
+  },
+  facebookAuth: {
+    boxSizing: 'border-box',
+    backgroundColor: '#4267B2',
+    borderRadius: 3,
+    boxShadow: "1px 2px 3px rgba(127,125,135, 0.8)",
+    paddingTop: 3,
+    paddingBottom: 3,
+    paddingLeft: 20,
+    paddingRight: 20,
+    margin: 2
+  },
+  facebookAuthText: {
+    color: '#fff',
+    display: 'table-cell',
+    textAlign: 'center',
+    verticalAlign: 'middle'
+  },
+  googleAuth: {
+    boxSizing: 'border-box',
+    backgroundColor: '#FF0000',
+    borderRadius: 3,
+    boxShadow: "1px 2px 3px rgba(127,125,135, 0.8)",
+    paddingTop: 3,
+    paddingBottom: 3,
+    paddingLeft: 20,
+    paddingRight: 20,
+    margin: 2,
+  },
+  googleAuthText: {
+    color: '#fff',
+    display: 'table-cell',
+    textAlign: 'center',
+    verticalAlign: 'middle'
+  },
+  submitButtonText: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  textInput: {
+    marginBottom: 2,
+    backgroundColor: '#fff'
+  }
+};
+
+export default styles;
---
+++ src/app/styles/mainStyles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+const mainStyles = StyleSheet.create({
+  feedBack: {
+    fontSize: 13,
+    color: '#ece3e2',
+    backgroundColor: '#494141',
+    marginTop: 2,
+    marginBottom: 2,
+    marginLeft: 5,
+    marginRight: 5,
+    display: 'block',
+    padding: 3,
+    borderRadius: 3
+  },
+  form: {
+    marginTop: 0,
+    marginBottom: 0,
+    marginRight: 50,
+    marginLeft: 50
+  },
+  titleMedium: {
+    display: 'block',
+    marginTop: 20,
+    marginBottom: 20,
+    textAlign: 'left',
+    fontSize: 14,
+    fontWeight: 'bold'
+  }
+});
+
+export default mainStyles;
